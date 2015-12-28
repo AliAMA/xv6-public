d168d2e8f811ce7416cab676e3d460b7b396bc18
Ali Asil <a.robot.254@gmail.com>
2015-12-28 23:34:37 +0330

    make was possible due to changing myfiletest.c file but file operations still not possible! running myfiletest caused: pid 4 myfiletest: trap 14 err 5 on cpu 1 eip 0xffffffff addr 0xffffffff--kill proc

--- myfiletest.c
+++ myfiletest.c
@@ -1,13 +1,21 @@
 #include "types.h"
-#include "defs.h"
+//#include "defs.h"
 #include "param.h"
 #include "fs.h"
 #include "file.h"
 #include "spinlock.h"
+#include "user.h"
+#include "stat.h"
 
 int
 main(){
-	fd = open("/hxo", O_CREATE);
+	//fd = open("/hxo", O_CREATE);
+	int fd;
+	fd = open("xy", 1);
 	write(fd, "abc", 3);
-	fd.close();
+	//link("x/y", "x/z");
+	//unlink("x/y");
+	//write(fd, "abc", 3);
+	//fd.close();
+	return(0);
 }
