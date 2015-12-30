5244f73fefc11a51c8016b95c8f637a213aefd5d
Ali Asil <a.robot.254@gmail.com>
2015-12-31 01:35:27 +0330

    File Operations added in kernel mode file open & close functions added

--- sysproc.c
+++ sysproc.c
@@ -112,19 +112,143 @@ sys_halt(void)
   return 0;
 }
 
+//
+static struct inode*
+create(char *path, short type, short major, short minor)
+{
+  uint off;
+  struct inode *ip, *dp;
+  char name[DIRSIZ];
+
+  if((dp = nameiparent(path, name)) == 0)
+    return 0;
+  ilock(dp);
+
+  if((ip = dirlookup(dp, name, &off)) != 0){
+    iunlockput(dp);
+    ilock(ip);
+    if(type == T_FILE && ip->type == T_FILE)
+      return ip;
+    iunlockput(ip);
+    return 0;
+  }
+
+  if((ip = ialloc(dp->dev, type)) == 0)
+    panic("create: ialloc");
+
+  ilock(ip);
+  ip->major = major;
+  ip->minor = minor;
+  ip->nlink = 1;
+  iupdate(ip);
+
+  if(type == T_DIR){  // Create . and .. entries.
+    dp->nlink++;  // for ".."
+    iupdate(dp);
+    // No ip->nlink++ for ".": avoid cyclic ref count.
+    if(dirlink(ip, ".", ip->inum) < 0 || dirlink(ip, "..", dp->inum) < 0)
+      panic("create dots");
+  }
+
+  if(dirlink(dp, name, ip->inum) < 0)
+    panic("create: dirlink");
+
+  iunlockput(dp);
+
+  return ip;
+}
+
+static int
+fdalloc(struct file *f)
+{
+  int fd;
+
+  for(fd = 0; fd < NOFILE; fd++){
+    if(proc->ofile[fd] == 0){
+      proc->ofile[fd] = f;
+      return fd;
+    }
+  }
+  return -1;
+}
+
+
+struct file* openfile(char* ipath, int iomode,int* inf)
+{
+  char *path;
+  int fd, omode;
+  struct file *f;
+  struct inode *ip;
+
+  path = ipath;
+  omode = iomode;
+
+  begin_op();
+
+  if(omode & O_CREATE){
+    ip = create(path, T_FILE, 0, 0);
+    if(ip == 0){
+      end_op();
+      return 0;
+    }
+  } else {
+    if((ip = namei(path)) == 0){
+      end_op();
+      return 0;
+    }
+    ilock(ip);
+    if(ip->type == T_DIR && omode != O_RDONLY){
+      iunlockput(ip);
+      end_op();
+      return 0;
+    }
+  }
+
+  if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
+    if(f)
+      fileclose(f);
+    iunlockput(ip);
+    end_op();
+    return 0;
+  }
+  iunlock(ip);
+  end_op();
+
+  f->type = FD_INODE;
+  f->ip = ip;
+  f->off = 0;
+  f->readable = !(omode & O_WRONLY);
+  f->writable = (omode & O_WRONLY) || (omode & O_RDWR);
+  *inf = fd;
+  return f;
+}
+
+int closefile(int ifd, struct file * inf){
+  int fd = ifd;
+  struct file *f;
+
+  f = inf;
+  proc->ofile[fd] = 0;
+  fileclose(f);
+  return 0;
+}
+
 int
 sys_getproc(void)
 {
   //struct proc* this_proc = proc;
   struct proc** out_proc = 0;
-  struct file* myf = filealloc();
+  struct file* myfile = 0 ;
   if (argptr(0, (void*)&out_proc,sizeof(out_proc)) < 0)
     return -1;
   *out_proc = proc;
   //filewrite(struct file *f, char *addr, int n)
+  int fd=0;
   char* addr = "/kt1.txt";
-  filewrite(myf, addr,2);
-  fileclose(myf);
+  myfile = openfile(addr, O_CREATE, &fd);
+  closefile(fd,myfile);
+  //filewrite(myf, addr,2);
+  //fileclose(myf);
 
   //sys_write(1,this_proc->kstack, sizeof(this_proc->kstack));
   return (*out_proc)->pid;
